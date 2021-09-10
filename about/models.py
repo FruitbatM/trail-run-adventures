c2bf5755c8f486fdbc79358356dc6f078f366b26
FruitbatM <mar.pavlovic@gmail.com>
2021-09-10 11:18:26 +0000

    Update models and urls

--- about/models.py
+++ about/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Team(models.Model):
 
-    name = models.CharField(max_length=75)
-    title = models.CharField(max_length=50)
-    about = models.CharField(max_length=500)
+    name = models.CharField(max_length=50)
+    title = models.CharField(max_length=75)
+    about = models.TextField(max_length=700)
 
     def __str__(self):
         return self.name
