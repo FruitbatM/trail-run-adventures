b1cc042073bf57b28e8429c4e4edad4b4e0676b0
FruitbatM <mar.pavlovic@gmail.com>
2021-09-11 18:05:48 +0000

    Add field image to instructor model

--- about/models.py
+++ about/models.py
@@ -8,7 +8,8 @@ class Instructor(models.Model):
 
     name = models.CharField(max_length=50)
     title = models.CharField(max_length=75)
-    about = models.TextField(max_length=700)
+    about = models.TextField(max_length=900)
+    image = models.ImageField(upload_to="instructors", null=True, blank=True)
 
     def __str__(self):
         return self.name
