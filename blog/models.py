1c35ce5db5dbfe1f27ac4ba8f818b65e54660d3f
FruitbatM <mar.pavlovic@gmail.com>
2021-09-15 14:11:52 +0000

    Amend blog model to upload images to media blog directory and make migrations

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,8 @@ class BlogPost(models.Model):
 
     author = models.CharField(max_length=50, null=True, blank=True)
     published_date = models.DateTimeField(auto_now_add=True)
-    header_image = models.ImageField()
+    # image will be uploaded to MEDIA_ROOT/blog
+    header_image = models.ImageField(upload_to="blog", null=True, blank=True)
     title = models.CharField(max_length=80)
     slug = models.SlugField(max_length=150, unique=True)
     intro_paragraph_1 = models.TextField(validators=[MinLengthValidator(100)],
@@ -21,12 +22,14 @@ class BlogPost(models.Model):
     subheading_2 = models.CharField(max_length=80)
     blog_content_2 = models.TextField(validators=[MinLengthValidator(250)])
     blog_content_3 = models.TextField(validators=[MinLengthValidator(250)])
-    blog_image_1 = models.ImageField()
+    # image will be uploaded to MEDIA_ROOT/blog
+    blog_image_1 = models.ImageField(upload_to="blog", null=True, blank=True)
     subheading_3 = models.CharField(max_length=80)
     blog_content_4 = models.TextField(validators=[MinLengthValidator(250)])
     blog_content_5 = models.TextField(validators=[MinLengthValidator(250)])
     blog_content_6 = models.TextField(validators=[MinLengthValidator(250)])
-    blog_image_2 = models.ImageField()
+    # image will be uploaded to MEDIA_ROOT/blog
+    blog_image_2 = models.ImageField(upload_to="blog", null=True, blank=True)
     subheading_4 = models.CharField(max_length=55, null=True, blank=True)
     blog_content_7 = models.TextField(validators=[MinLengthValidator(250)],
                                       null=True, blank=True)
