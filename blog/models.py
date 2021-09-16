56796bbaf8659292981282917e7606dd11a67974
FruitbatM <mar.pavlovic@gmail.com>
2021-09-16 08:23:06 +0000

    Update blog model field argument blank to True to allow the field to be blank

--- blog/models.py
+++ blog/models.py
@@ -16,18 +16,24 @@ class BlogPost(models.Model):
     intro_paragraph_1 = models.TextField(validators=[MinLengthValidator(100)],
                                          max_length=500)
     intro_paragraph_2 = models.TextField(validators=[MinLengthValidator(100)],
-                                         max_length=500)
-    subheading_1 = models.CharField(max_length=80)
-    blog_content_1 = models.TextField(validators=[MinLengthValidator(250)])
-    subheading_2 = models.CharField(max_length=80)
-    blog_content_2 = models.TextField(validators=[MinLengthValidator(250)])
-    blog_content_3 = models.TextField(validators=[MinLengthValidator(250)])
+                                         max_length=500, null=True, blank=True)
+    subheading_1 = models.CharField(max_length=80, null=True, blank=True)
+    blog_content_1 = models.TextField(validators=[MinLengthValidator(250)],
+                                      null=True, blank=True)
+    subheading_2 = models.CharField(max_length=80, null=True, blank=True)
+    blog_content_2 = models.TextField(validators=[MinLengthValidator(250)],
+                                      null=True, blank=True)
+    blog_content_3 = models.TextField(validators=[MinLengthValidator(250)],
+                                      null=True, blank=True)
     # image will be uploaded to MEDIA_ROOT/blog
     blog_image_1 = models.ImageField(upload_to="blog", null=True, blank=True)
-    subheading_3 = models.CharField(max_length=80)
-    blog_content_4 = models.TextField(validators=[MinLengthValidator(250)])
-    blog_content_5 = models.TextField(validators=[MinLengthValidator(250)])
-    blog_content_6 = models.TextField(validators=[MinLengthValidator(250)])
+    subheading_3 = models.CharField(max_length=70, null=True, blank=True)
+    blog_content_4 = models.TextField(validators=[MinLengthValidator(250)],
+                                      null=True, blank=True)
+    blog_content_5 = models.TextField(validators=[MinLengthValidator(250)],
+                                      null=True, blank=True)
+    blog_content_6 = models.TextField(validators=[MinLengthValidator(250)],
+                                      null=True, blank=True)
     # image will be uploaded to MEDIA_ROOT/blog
     blog_image_2 = models.ImageField(upload_to="blog", null=True, blank=True)
     subheading_4 = models.CharField(max_length=55, null=True, blank=True)
