7e2b55da847729539f559ab68b154febf1ddf163
FruitbatM <mar.pavlovic@gmail.com>
2021-09-16 14:37:07 +0000

    Updated index function to view blog posts on the home page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from blog.models import BlogPost
 
 
 def index(request):
     """
     A view to return the index page
     """
-    return render(request, 'home/index.html')
+
+    blog_home = BlogPost.objects.all().order_by('pk')[:2]
+
+    context = {
+        'blog_home': blog_home
+    }
+
+    return render(request, 'home/index.html', context)
