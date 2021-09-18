df26aca5d0976a8bd719bf9fb09d5c0c550bfe76
FruitbatM <mar.pavlovic@gmail.com>
2021-09-18 07:32:24 +0000

    Customize admin

--- products/admin.py
+++ products/admin.py
@@ -2,8 +2,47 @@ from django.contrib import admin
 from .models import Product, Category, Level, Itinerary, ItineraryDay
 
 
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Level)
-admin.site.register(Itinerary)
-admin.site.register(ItineraryDay)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'sku',
+        'category',
+        'level',
+        'price',
+        'rating',
+        'image_1',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class LevelAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+class ItineraryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+class ItineraryDayAdmin(admin.ModelAdmin):
+    list_display = (
+        'itinerary',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Level, LevelAdmin)
+admin.site.register(Itinerary, ItineraryAdmin)
+admin.site.register(ItineraryDay, ItineraryDayAdmin)
