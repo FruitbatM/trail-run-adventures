73de5507ddeafd421d12df0c79729b186be92136
FruitbatM <mar.pavlovic@gmail.com>
2021-09-18 13:46:11 +0000

    Add product details functionality

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -14,3 +14,18 @@ def all_products(request):
     }
 
     return render(request, 'products/all_products.html', context)
+
+
+def product_detail(request, product_id):
+    """
+    A view to display a single product details
+    """
+
+    all_products = Product.objects.filter(is_holiday=False)
+    product = get_object_or_404(all_products, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
