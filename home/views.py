7ed2e277714006fca10f7d4efd4e9e2e3c642887
FruitbatM <mar.pavlovic@gmail.com>
2021-09-20 14:57:27 +0000

    Add Products categories on the home template

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from blog.models import BlogPost
+from products.models import Product
 
 
 def index(request):
@@ -8,9 +9,12 @@ def index(request):
     """
 
     blog_home = BlogPost.objects.all().order_by('pk')[:2]
+    shop_home = Product.objects.filter(is_holiday=False,
+                                       id__in=(7, 24, 13))[:3]
 
     context = {
-        'blog_home': blog_home
+        'blog_home': blog_home,
+        'shop_home': shop_home,
     }
 
     return render(request, 'home/index.html', context)
