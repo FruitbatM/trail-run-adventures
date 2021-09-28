07ef88b0590ae4fae674eee1eae7aa6d6bcb32a7
FruitbatM <mar.pavlovic@gmail.com>
2021-09-28 20:25:08 +0000

    Add blog comments form, update view and create blog comment template

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
-from .models import BlogPost
+from .models import BlogPost, Comment
+from .forms import CommentForm
 
 
 def blog(request):
-    """
-    A view to return the Blog page
-    """
+    """ A view to return the Blog page """
 
     blogs = BlogPost.objects.all()
 
@@ -18,14 +18,26 @@ def blog(request):
 
 
 def blog_post(request, slug):
-    """
-    Add blog post to the blog
-    """
+    """ A view to add blog post to the blog """
 
     blog = get_object_or_404(BlogPost, slug=slug)
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = blog_post
+            new_comment.save()
+            comment_form = CommentForm()
+            messages.success(request, 'Comment was successfully posted.')
+    else:
+        comment_form = CommentForm()
 
     context = {
         'blog': blog,
+        'blog_post': blog_post,
+        'comment_form': comment_form,
     }
 
     return render(request, 'blog/blog_post.html', context)
