bb5fb0c9a7ae682a446c9d8ee2bec4fe03b91cc6
FruitbatM <mar.pavlovic@gmail.com>
2021-09-30 17:21:49 +0000

    Add url and fix PEP8 requirement for too long lines

--- blog/forms.py
+++ blog/forms.py
@@ -19,16 +19,26 @@ class BlogForm(forms.ModelForm):
 
         self.fields['title'].widget.attrs['autofocus'] = True
         self.fields['slug'].widget.attrs['placeholder'] = 'ending of the link'
-        self.fields['intro_paragraph_1'].widget.attrs['placeholder'] = '100 - 500 characters'
-        self.fields['intro_paragraph_2'].widget.attrs['placeholder'] = '100 - 500 characters'
-        self.fields['subheading_1'].widget.attrs['placeholder'] = 'max 80 characters'
-        self.fields['blog_content_1'].widget.attrs['placeholder'] = 'min 250 characters'
-        self.fields['subheading_2'].widget.attrs['placeholder'] = 'max 80 characters'
-        self.fields['blog_content_2'].widget.attrs['placeholder'] = 'min 250 characters'
-        self.fields['blog_content_3'].widget.attrs['placeholder'] = 'min 250 characters'
-        self.fields['subheading_3'].widget.attrs['placeholder'] = 'max 80 characters'
-        self.fields['blog_content_4'].widget.attrs['placeholder'] = 'min 250 characters'
-        self.fields['blog_content_5'].widget.attrs['placeholder'] = 'min 250 characters'
+        self.fields['intro_paragraph_1'].widget.attrs[
+                    'placeholder'] = '100 - 500 characters'
+        self.fields['intro_paragraph_2'].widget.attrs[
+                    'placeholder'] = '100 - 500 characters'
+        self.fields['subheading_1'].widget.attrs[
+                    'placeholder'] = 'max 80 characters'
+        self.fields['blog_content_1'].widget.attrs[
+                    'placeholder'] = 'min 250 characters'
+        self.fields['subheading_2'].widget.attrs[
+                    'placeholder'] = 'max 80 characters'
+        self.fields['blog_content_2'].widget.attrs[
+                    'placeholder'] = 'min 250 characters'
+        self.fields['blog_content_3'].widget.attrs[
+                    'placeholder'] = 'min 250 characters'
+        self.fields['subheading_3'].widget.attrs[
+                    'placeholder'] = 'max 80 characters'
+        self.fields['blog_content_4'].widget.attrs[
+                    'placeholder'] = 'min 250 characters'
+        self.fields['blog_content_5'].widget.attrs[
+                    'placeholder'] = 'min 250 characters'
 
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'add-blog-form-field'
--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.blog, name='basecampblog'),
     path('<int:post_id>/', views.blog_post, name='blog_post'),
+    path('add_blog/', views.add_blog, name='add_blog'),
 ]
