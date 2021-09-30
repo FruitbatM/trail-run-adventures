ce968d10bc211dfb5dd5c36e0ef7696b4c98262f
FruitbatM <mar.pavlovic@gmail.com>
2021-09-30 17:14:40 +0000

    Create add blog functionality add views, form and template

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,34 @@
 from django import forms
-from .models import Comment
+from .models import Comment, BlogPost
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('name', 'body')
+
+
+class BlogForm(forms.ModelForm):
+
+    class Meta:
+        model = BlogPost
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['title'].widget.attrs['autofocus'] = True
+        self.fields['slug'].widget.attrs['placeholder'] = 'ending of the link'
+        self.fields['intro_paragraph_1'].widget.attrs['placeholder'] = '100 - 500 characters'
+        self.fields['intro_paragraph_2'].widget.attrs['placeholder'] = '100 - 500 characters'
+        self.fields['subheading_1'].widget.attrs['placeholder'] = 'max 80 characters'
+        self.fields['blog_content_1'].widget.attrs['placeholder'] = 'min 250 characters'
+        self.fields['subheading_2'].widget.attrs['placeholder'] = 'max 80 characters'
+        self.fields['blog_content_2'].widget.attrs['placeholder'] = 'min 250 characters'
+        self.fields['blog_content_3'].widget.attrs['placeholder'] = 'min 250 characters'
+        self.fields['subheading_3'].widget.attrs['placeholder'] = 'max 80 characters'
+        self.fields['blog_content_4'].widget.attrs['placeholder'] = 'min 250 characters'
+        self.fields['blog_content_5'].widget.attrs['placeholder'] = 'min 250 characters'
+
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'add-blog-form-field'
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
-from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 from .models import BlogPost, Comment
-from .forms import CommentForm
+from .forms import CommentForm, BlogForm
 
 
 def blog(request):
@@ -43,3 +43,33 @@ def blog_post(request, post_id):
     }
 
     return render(request, 'blog/blog_post.html', context)
+
+
+@login_required
+def add_blog(request):
+    """ Add a post to the blog """
+
+    if not request.user.is_superuser:
+        messages.error(request, 'Only TRΛIL RUN ΛDVENTURES Team has permission to add a Blog post.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        blog_form = BlogForm(request.POST)
+        if blog_form.is_valid():
+            # Create Blog object but don't save to database yet
+            new_blog = blog_form.save(commit=False)
+            # Assign the auther to the new blog and save it
+            new_blog.author = request.user
+            new_blog.save()
+            messages.success(request, 'Successfully added a blog post!')
+        else:
+            messages.error(request, 'Failed to add the blog post. \
+                           Please ensure the form is valid.')
+    else:
+        blog_form = BlogForm()
+
+    context = {
+        'blog_form': blog_form,
+    }
+
+    return render(request, 'blog/add_blog.html', context)
