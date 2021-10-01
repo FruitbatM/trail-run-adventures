27679e45a4e1af0f68b0df14355eb82c46c973ea
FruitbatM <mar.pavlovic@gmail.com>
2021-10-01 14:38:04 +0000

    Correct too long lines according to PEP8 compliance

--- cart/views.py
+++ cart/views.py
@@ -13,9 +13,9 @@ def view_cart(request):
 
 
 def add_to_cart(request, item_id):
-    """ 
-    Add a quantity of the specified product / holiday adventure 
-    to the shopping cart 
+    """
+    Add a quantity of the specified product / holiday adventure
+    to the shopping cart
     """
 
     product = get_object_or_404(Product, pk=item_id)
@@ -30,17 +30,22 @@ def add_to_cart(request, item_id):
         if item_id in list(cart.keys()):
             if size in cart[item_id]['items_by_size'].keys():
                 cart[item_id]['items_by_size'][size] += quantity
-                messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {cart[item_id]["items_by_size"][size]}')
+                messages.success(request, f'Updated size \
+                                {size.upper()} {product.name} quantity to \
+                                {cart[item_id]["items_by_size"][size]}')
             else:
                 cart[item_id]['items_by_size'][size] = quantity
-                messages.success(request, f'Added size {size.upper()} {product.name} to your cart')
+                messages.success(request, f'Added size \
+                                {size.upper()} {product.name} to your cart')
         else:
             cart[item_id] = {'items_by_size': {size: quantity}}
-            messages.success(request, f'Added size {size.upper()} {product.name} to your cart')
+            messages.success(request, f'Added size \
+                            {size.upper()} {product.name} to your cart')
     else:
         if item_id in list(cart.keys()):
             cart[item_id] += quantity
-            messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
+            messages.success(request, f'Updated \
+                            {product.name} quantity to {cart[item_id]}')
         else:
             cart[item_id] = quantity
             messages.success(request, f'Added {product.name} to your cart')
@@ -65,19 +70,24 @@ def adjust_cart(request, item_id):
     if size:
         if quantity > 0:
             cart[item_id]['items_by_size'][size] = quantity
-            messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {cart[item_id]["items_by_size"][size]}')
+            messages.success(request, f'Updated size \
+                            {size.upper()} {product.name} quantity to \
+                            {cart[item_id]["items_by_size"][size]}')
         else:
             del cart[item_id]['items_by_size'][size]
             if not cart[item_id]['items_by_size']:
                 cart.pop(item_id)
-            messages.success(request, f'Removed size {size.upper()} {product.name} from your cart')
+            messages.success(request, f'Removed size \
+                            {size.upper()} {product.name} from your cart')
     else:
         if quantity > 0:
             cart[item_id] = quantity
-            messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
+            messages.success(request, f'Updated \
+                            {product.name} quantity to {cart[item_id]}')
         else:
             cart.pop(item_id)
-            messages.success(request, f'Removed {product.name} from your cart')
+            messages.success(request, f'Removed \
+                            {product.name} from your cart')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -97,7 +107,8 @@ def remove_from_cart(request, item_id):
             del cart[item_id]['items_by_size'][size]
             if not cart[item_id]['items_by_size']:
                 cart.pop(item_id)
-            messages.success(request, f'Removed size {size.upper()} {product.name} from your cart')
+            messages.success(request, f'Removed size \
+                            {size.upper()} {product.name} from your cart')
         else:
             cart.pop(item_id)
             messages.success(request, f'Removed {product.name} from your cart')
