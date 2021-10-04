645810523bed38e58056a8de452c3492315446d6
FruitbatM <mar.pavlovic@gmail.com>
2021-10-04 15:18:00 +0000

    Remove secret key and set debug

--- trail_run_adventures/settings.py
+++ trail_run_adventures/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@%vs5mmh0_!8u4-p!64dp3r^!_8g$%hlmzemuz45)$2jk-#5)7'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['trail-run-adventures.herokuapp.com', 'localhost']
 
